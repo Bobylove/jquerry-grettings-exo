68d92ceb302bc91df470a1db93de384b79efd616
Fred <delon.frederic.pro.job@gmail.com>
2016-10-07 10:56:31 +0200

    fred End commit

--- app.js
+++ app.js
@@ -2,16 +2,11 @@ $(".infos").click(function(){
 	var first = $('#first_name').val();
 	var last = $('#last_name').val();
 	var ville = $('#city').val();
-	// var nom = $('#username').html(first + " " + last + " " + ville);
-	
-
 	var obj = {
 		prenom: first,
 		nom1: last,
 		city: ville
-
 	};
 	var hello = (obj.prenom + " " + obj.nom1 + " " + obj.city);
 	$('#username').html(hello);
-
 });
